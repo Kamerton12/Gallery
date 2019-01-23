062f7047b89314428b7cdfa8c4a7e54a0abea60b
MaxLaptop <s312@tut.by>
2019-01-23 16:05:36 +0300

    now it really works

---
+++ app/src/main/java/com/project/transparentactivity/ImageItem.java
@@ -0,0 +1,30 @@
+package com.project.transparentactivity;
+
+import android.graphics.Bitmap;
+
+public class ImageItem
+{
+    String image;
+    String text;
+
+    public ImageItem(String image, String text) {
+        this.image = image;
+        this.text = text;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
